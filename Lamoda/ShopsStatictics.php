6e1668acfa6f31ed5456768f685968339ed57a20
Alexander Rusak <375298388602@mail.ru>
2018-06-13 23:27:23 +0300

    i'm at begin to make exeptions version 1.2.4

--- Lamoda/ShopsStatictics.php
+++ Lamoda/ShopsStatictics.php
@@ -16,45 +16,83 @@ try {
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $res=curl_exec($ch);
-  throw new Exception("Execution if failed");
-} catch (Exception $ExecutionErr) {
+if ($res==FALSE) {
+	throw new Exception("The execution was not successful: "." file ".__FILE__." on line ".__LINE__, 1);
+	exit();
+	}
+}
+catch (Exception $ExecutionErr) {
 	echo $ExecutionErr->getMessage();
 }
-
-  
 curl_close($ch);
-
 $doc=phpQuery::newDocument($res);
-if (is_null($doc)) 
-	echo "Error";
+			try {
 $pages=$doc->find('.paging')->find('li:last')->text();
+if (is_null($pages) || $pages==0) {
+throw new Exception("Parse error: "." file ".__FILE__." on line ".__LINE__, 1);
+				                  }
+				}				 
+			catch (Exception $ParseErr) {
+			echo $ParseErr->getMessage();
+			exit();
+				}
 for ($i=0; $i<$pages ; ++$i) { 
 	$url='https://www.vitrini.by/places/shopping/clothes/'.$i."/";
     $ch=curl_init($url);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$resShops=curl_exec($ch);
+			try{
+			if ($resShops==FALSE) {
+			throw new Exception("The execution was not successful: "." file ".__FILE__." on line ".__LINE__, 1);
+			exit();
+				}
+}				
+			catch (Exception $ExecutionShopsErr) {
+			echo $ExecutionShopsErr->getMessage();
+}
 	curl_close($ch);
 	$doc=phpQuery::newDocument($resShops);
-$shops=$doc->find('.store-box__title');
+	try {
+		$shops=$doc->find('.store-box__title');
+		if ($shops=="") {
+		throw new Exception("Parse error: "." file ".__FILE__." on line ".__LINE__, 1);
+		exit();
+				                  }
 	foreach ($shops as $key=>$shop) {
 		$pqShop=pq($shop);
 		$textShops[]=$pqShop->text()." ";
 	}
 $raitings=$doc->find('.raiting')->find('span');
+		if ($raitings=="") {
+		throw new Exception("Parse error: "." file ".__FILE__." on line ".__LINE__, 1);
+		exit();
+				                  }
 	foreach ($raitings as $key=>$raiting) {
 		$pqRaiting=pq($raiting);
 		$textRaiting[]=$pqRaiting->text()." ";
 	}
 $responses=$doc->find('.response')->find('span');
+if ($responses=="") {
+		throw new Exception("Parse error: "." file ".__FILE__." on line ".__LINE__, 1);
+		exit();
+				                  }
 	foreach ($responses as $key=>$response) {
 		$pqResponse=pq($response);
 		$textResponses[]=$pqResponse->text()." ";
 	}
 $responsesUrl=$doc->find('.response')->find('a');
+if ($responsesUrl=="") {
+		throw new Exception("Parse error: "." file ".__FILE__." on line ".__LINE__, 1);
+		exit();
+				                  }
 	foreach ($responsesUrl as $key=>$resUrl) {
 		$pqUrl=pq($resUrl);
 		$textUrl[]=$pqUrl->attr('href');
+	}
+	 } 
+	catch (Exception $ExexptionParseShops) {
+		echo $ExexptionParseShops->getMessage();
 	}
 }
 $todayRaiting=(string)date("d-m-Y").' Raiting.txt';
@@ -66,16 +104,17 @@ file_put_contents('Raitings\\'.$todayRaiting, $textRaiting);
 file_put_contents('Shops\\'.$todayShops, $textShops);
 file_put_contents('Responses\\'.$todayResponses, $textResponses);
 $cts=count($textShops);//counts of text shops
-echo $cts;
 for($i=0;$i<$cts;++$i)
 		{
 			$fileTotal[$i]= $textShops[$i]." ".$textRaiting[$i]." ".$textResponses[$i].",";
 		}
 		file_put_contents('Total\\'.$todayTotal, $fileTotal);
 		file_put_contents('Total\\'.$todayTotalcsv, $fileTotal);
-getStatistics($textUrl,$textShops,$textResponses);
+//getStatistics($textUrl,$textShops,$textResponses);
+
 }
 shopsName();
+
 $time = microtime(true) - $start;
 printf('Скрипт выполнялся %.4F мин.', $time/60);
 ?>
